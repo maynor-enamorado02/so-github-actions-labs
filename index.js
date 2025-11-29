2afe77c3e491a499a33de8fdf0caddd466bbd910
maynor-enamorado02 <mynor.enamorado@unitec.edu>
2025-11-29 15:32:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function greet(name = 'World') {
+  return `Hello, ${name}! from CI Pipeline`;
+}
 
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { greet, add };
+
+if (require.main === module) {
+  console.log(greet('CI Pipeline User'));
+  console.log('Suma de 2 + 3:', add(2, 3));
+}
