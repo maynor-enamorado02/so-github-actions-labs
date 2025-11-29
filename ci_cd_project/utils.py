15cd84ffcd3ee14105a61904a6e52c4f7a270956
maynor-enamorado02 <mynor.enamorado@unitec.edu>
2025-11-29 17:09:18 -0600

    Add file read and write utility functions

--- ci_cd_project/utils.py
+++ ci_cd_project/utils.py
@@ -1,1 +1,9 @@
 
+def write_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+
+def read_file(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
